918b6708e9d405079997de95ea8c3aeac92e9863
Nivesh Singh <nivesh2@gmail.com>
2016-03-20 12:53:30 +0530

    Package.JSON updated with multer

--- server/app/controller/feedback.controller.js
+++ server/app/controller/feedback.controller.js
@@ -1,20 +1,4 @@
 'use strict';
-/*var Telenode = require('telenode');
-var client = new Telenode(Telenode.providers.twilio);
- 
-client.credentials({
-  sid: 'ACc302210e3dc657a16d79625d8521f4d2',
-authToken: '3b14a4c7413647d2d367a2bb308027b2'
-});*/
- 
- var accountSid = 'ACc302210e3dc657a16d79625d8521f4d2'; 
-var authToken = '3b14a4c7413647d2d367a2bb308027b2'; 
- 
-//require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
- 
-
-
 
 module.exports = (function(){
    const config = require('../../config/config'),
@@ -22,37 +6,27 @@ module.exports = (function(){
           analytics= require('../../app/model/analytics.model.js'),
           debug = require('debug')('main:feedback-controller'),
           that = {};
-          
+
     that.add = (req,res,next) => {
-   
-      // analytics.insert(req.body.data.feedback);
+
+         analytics.insert(req.body.data.feedback);
          feedback.insert(req.body.data,function(err,result){
          if(err){
              next(err);
-         } else{ 
-             
+         } else{
+
          res.json({
             'success':1,
             'message':'feedback inserted',
             'data':result
-            });   
+            });
          }
-         client.messages.create({ 
-             from:"+919968500931",
-        	to: "+918800691089",  
-        	body: "Test",   
-        }, function(err, message) { 
-            if(err){
-        	console.log(err);
-            }
-            console.log(message);
-        });
          global.io.emit('newPush', req.body.data);
-         
+
       });
-     
+
     };
-    
+
     that.fetchById = (req,res,next) => {
         feedback.fetchById(req.params.itemId,function(err,result){
             if(err){
@@ -62,10 +36,10 @@ module.exports = (function(){
                     success: 1,
                     message:'Feedback Found',
                     'data': result,
-            });  
+            });
         })
     };
-    
+
     that.delete = (req,res,next) => {
       feedback.delete({"itemId":req.query.itemId,"phonenumber":req.query.phonenumber},function(err,result){
           if(err){
@@ -75,10 +49,10 @@ module.exports = (function(){
                     success: 1,
                     message:'Feedback Deleted',
                     'data': result,
-            });  
-      })  
+            });
+      })
     };
-    
+
     that.findInOneDay=  function(req,res,next){
         analytics.getinOneDay(function(err,result){
             if(err){
@@ -88,11 +62,11 @@ module.exports = (function(){
                     success: 1,
                     message:'Recent Feedback Sent',
                     'data': result,
-            });  
+            });
         });
     };
-    
-    
-    
+
+
+
     return that;
 })();
--- server/app/model/analytics.model.js
+++ server/app/model/analytics.model.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 
 module.exports= (function(){
     const config = require('../../config/config'),
@@ -7,25 +7,22 @@ module.exports= (function(){
          // item = require('../../app/model/item.model.js'),
           debug = require('debug')('main:feedback-mongo'),
           that={};
-   
+
     that.insert = (_data) =>{
-        
+
         debug(_data);
         const data = new Analytics(_data);
         data.save((err)=>{
             if(err){
                 debug('Error: Analytics insertion failure');
-                //cb(err);
-                
             }
-           // cb(null,data);
         });
-    
+
     };
  //   db.collection.find({ $where: function () { return Date.now() - this._id.getTimestamp() < (24 * 60 * 60 * 1000)  }  })
     that.getinOneDay = (cb) => {
         Analytics.find({ $where: function () {
-            return Date.now() - this._id.getTimestamp() < (24 * 60 * 60 * 1000) 
+            return Date.now() - this._id.getTimestamp() < (24 * 60 * 60 * 1000)
             } },(err,data)=>{
            if(err){
                 debug('Error: Data retrival failure');
@@ -34,11 +31,6 @@ module.exports= (function(){
            cb(null,data);
         });
     };
-        
-
-   
-    
-    
     return that;
-    
+
 })();
