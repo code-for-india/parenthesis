379168c4bf6cfd1f210dacc72e2251c0dd90b842
Nivesh Singh <nivesh2@gmail.com>
2016-03-20 12:56:31 +0530

    Final Commit

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    port:8081,
-    db:'mongodb://nivesh:just_4_now@ds055584.mongolab.com:55584/api',
+    port:8080,
+    db:'mongodb://username:password@DatabaseURL:PORT/databaseName',
     secret:'ilovenodejs',
     tokenExpiresIn:'1d'
-}; 
+};
